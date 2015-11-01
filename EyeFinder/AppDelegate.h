398e51e442d06ee03a361279b108a46f63a24474
Craig Thrall <cthrall@new-host-2.home>
2015-11-01 16:59:44 -0500

    Initial Commit

---
+++ EyeFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EyeFinder
+//
+//  Created by Craig Thrall on 11/1/15.
+//  Copyright © 2015 Craig Thrall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EyeFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EyeFinder
+//
+//  Created by Craig Thrall on 11/1/15.
+//  Copyright © 2015 Craig Thrall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
