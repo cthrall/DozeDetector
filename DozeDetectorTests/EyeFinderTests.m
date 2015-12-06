285984bf8cfcc645c9a718c62b329ec219340f16
Craig Thrall <cthrall@craigs-mac-mini.home>
2015-12-05 23:46:22 -0500

    More renaming

---
+++ DozeDetectorTests/EyeFinderTests.m
@@ -0,0 +1,39 @@
+//
+//  EyeFinderTests.m
+//  EyeFinderTests
+//
+//  Created by Craig Thrall on 11/1/15.
+//  Copyright © 2015 Craig Thrall. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EyeFinderTests : XCTestCase
+
+@end
+
+@implementation EyeFinderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
